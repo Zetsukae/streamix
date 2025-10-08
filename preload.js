18d55fbf9a22f57b57c2df79066c7c0f5a33ba5c
Zetsukae <dangeloalexis2@gmail.com>
2025-10-08 20:11:56 +0200

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimize: () => ipcRenderer.invoke('minimize-window'),
-  close: () => ipcRenderer.invoke('close-window'),
-  showDialog: (title, message) => ipcRenderer.invoke('show-dialog', title, message)
-})
